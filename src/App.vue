46dc1edd691a0d973e96aaada3c76e8ba9f55c51
chris097 <chrisfidel.international@gmail.com>
2023-01-10 05:00:11 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-blue-600 mt-2"> Hello world</div>
+  <MainNav />
+</template>
+
+<script>
+import MainNav from './components/MainNav.vue';
+
+export default {
+  name: "App",
+  components:{
+    MainNav: MainNav
+  }
+}
+</script>
+
