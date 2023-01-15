2080f6faceb486382cefbe4566e05127b7aff6dc
chris097 <chrisfidel.international@gmail.com>
2023-01-15 22:06:16 +0100

    feature header

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,14 @@
     <header class="w-full text-sm">
         <div class="fixed top-0 left-0 h-16 w-full">
             <div class="flex h-full flex-nowrap border-b border-solid border-brand-gray-1 mx-auto px-8">
-                <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+                <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+                <nav class="ml-12 h-full">
+                    <ul class="flex list-none h-full">
+                        <li class="h-full ml-9 first:ml-0" v-for="menuItem in menuItems" :key="menuItem">
+                            <a class="flex h-full items-center py-2.5" href="">{{ menuItem }}</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </header>
@@ -13,7 +20,16 @@ export default {
     name: "MainNav",
     data() {
         return {
-            company: "Job Career"
+            company: "Job Career",
+            url: "https://careers.google.com",
+            menuItems: [
+                "Teams",
+                "Locations",
+                "Life at Job Career",
+                "How we hire",
+                "Students",
+                "Jobs"
+            ]
         }
     }
 }
--- src/tests/unit/components/MainNav.test.js
+++ src/tests/unit/components/MainNav.test.js
@@ -5,6 +5,23 @@ import MainNav from "@/components/MainNav.vue"
 describe("MainNav", () => {
     it("display company name", () => {
         render(MainNav);
-        screen.debug()
+        const companyName = screen.getByText("Job Career");
+        expect(companyName).toBeInTheDocument();
+    });
+
+    it("display menu items for navigation", () => {
+        render(MainNav)
+        const navigationMenuitems = screen.getAllByRole("listitem")
+        const navigationMenuText = navigationMenuitems.map((item) => (
+            item.textContent
+        ))
+        expect(navigationMenuText).toEqual([
+            "Teams",
+            "Locations",
+            "Life at Job Career",
+            "How we hire",
+            "Students",
+            "Jobs"
+        ])
     })
 })
