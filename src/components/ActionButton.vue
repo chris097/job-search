9f7d6bf64f9dfb92393a49a7f9f3cbfcf928e64d
chris097 <chrisfidel.international@gmail.com>
2023-01-17 03:52:47 +0100

    feature header

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <button class="rounded bg-brand-blue-1 text-white border-0 px-5 py-3 font-medium hover:shadow-blue">Sign In</button>
+</template>
+
+<script>
+export default {
+    name: "ActionButton"
+}
+</script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -10,14 +10,21 @@
                         </li>
                     </ul>
                 </nav>
+                <div class="ml-auto flex items-center h-full py-2.5">
+                    <action-button />
+                </div>
             </div>
         </div>
     </header>
 </template>
 
 <script>
+    import ActionButton from './ActionButton.vue';
 export default {
     name: "MainNav",
+    components: {
+        ActionButton
+    },
     data() {
         return {
             company: "Job Career",
